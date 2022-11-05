ff3ccd02a6bf237742f011cb98682a702ff6d603
SHGroup <kshgroup@kshgroup.kr>
2022-11-06 01:24:53 +0900

    feat: add CORS support

--- haru/app.py
+++ haru/app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from haru.story import story_router
 from haru.user import user_router
 from haru.utils.db import init_database
+
+
+def init_middlewares(app: FastAPI):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 def init_routers(app: FastAPI):
@@ -13,6 +24,7 @@ def init_routers(app: FastAPI):
 def create_app() -> FastAPI:
     app = FastAPI()
 
+    init_middlewares(app)
     init_routers(app)
     init_database()
 
