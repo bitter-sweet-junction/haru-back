2c46069053cd53e3a1de40c5198e1678cfcd1eb0
SHGroup <kshgroup@kshgroup.kr>
2022-11-06 06:20:15 +0900

    build: fix some docker command for local test

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -5,8 +5,9 @@ docker build . -t haru-back
 
 # run
 docker run \
-    -d --rm \
+    --rm \
     -p 1213:80 \
     --env-file .env \
+    -e GOOGLE_APPLICATION_CREDENTIALS=/data/credentials.json \
     -v $(pwd)/data:/data \
     haru-back
