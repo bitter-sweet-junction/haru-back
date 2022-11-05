bfa1b7bfcf49f62a0ef2939f8f5bfb867b9dfa9a
SHGroup <kshgroup@kshgroup.kr>
2022-11-06 01:48:15 +0900

    feat: now get_current_user func return user model

--- haru/utils/auth.py
+++ haru/utils/auth.py
@@ -4,6 +4,9 @@ import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
+from haru.utils.db import get_database
+from haru.model import User
+
 JWT_TOKEN = os.environ.get("HARU_JWT_TOKEN")
 oauth2 = OAuth2PasswordBearer(tokenUrl="/authorize/google")
 
@@ -12,16 +15,20 @@ def create_token(user_id: str) -> str:
     return jwt.encode({"user_id": user_id}, JWT_TOKEN, algorithm="HS256")
 
 
-def get_current_user_id(token: str = Depends(oauth2)) -> str:
+def get_current_user(token: str = Depends(oauth2)) -> User:
+    user = None
     try:
         result = jwt.decode(token, JWT_TOKEN, algorithms="HS256")
-        return result.get("user_id")
+        user_id = result.get("user_id")
+        user = get_database().user.find_one({"_id": user_id})
     except:
         pass
 
-    raise HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail="Auth failed",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Auth failed",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    return user
