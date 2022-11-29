364f7266398a7060172bb81386dfe3a6e290f0c9
SHGroup <kshgroup@kshgroup.kr>
2022-11-29 09:51:01 +0900

    feat: stability api를 활용하도록 수정

--- haru-back-generate-image/main.py
+++ haru-back-generate-image/main.py
@@ -2,11 +2,11 @@ import base64
 import json
 import os
 
-import requests
 from google.cloud.storage import Client
 from pymongo import MongoClient
+from stability_sdk.client import StabilityInference
 
-IMAGE_GENERATOR_HOST = os.environ.get("IMAGE_GENERATOR_HOST")
+STABILITY_KEY = os.environ.get("STABILITY_KEY")
 MONGODB_URI = os.environ.get("MONGODB_URI")
 
 client = Client()
@@ -21,6 +21,17 @@ def upload_to_gcs(bucket: str, name: str, content: bytes, *, prefix: str = "") -
     return blob.public_url
 
 
+def inference(title: str, content: str) -> bytes:
+    prompt = f"A Cosy image of {title}. highly detailed, art by Makoto Shinkai, Trending on artstation"
+    answers = StabilityInference(key=STABILITY_KEY).generate(prompt=prompt)
+    for resp in list(answers):
+        for artifact in resp.artifacts:
+            if artifact.type == 1: # generation.ARTIFACT_IMAGE
+                return artifact.binary
+
+    return b""
+
+
 def entrypoint(event, context):
     data = json.loads(base64.b64decode(event["data"]).decode())
     story_id = data["_id"]
@@ -28,12 +39,8 @@ def entrypoint(event, context):
     title = data["title"]
     description = data["description"]
 
-    content = {"title": title, "content": description}
-    result = requests.post(f"{IMAGE_GENERATOR_HOST}/text2img", json=content)
-    if result.status_code != 200:
-        raise RuntimeError("Failed to generate image")
-
+    content = inference(title=title, content=description)
     picture_url = upload_to_gcs(
-        bucket="haru-image-store", prefix=user_id, name=f"picture_{story_id}", content=result.content
+        bucket="haru-image-store", prefix=user_id, name=f"picture_{story_id}", content=content
     )
     db.story.update_one({"_id": story_id}, {"$set": {"pictureUrl": picture_url}})
